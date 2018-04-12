a70564708d4070bd9fafbf07c29689bb3439edfc
pbdevch <philipp.bergamin@gmail.com>
2018-04-12 17:21:20 +0200

    Added example config (as the normal file will be encrypted soon)

---
+++ config.example.py
@@ -0,0 +1,27 @@
+"""
+Is this file are the configurations for debug and production mode defined. In the debug config is also explained for what the properties are for
+"""
+
+debug_config = {
+    "botParent": "<YOUR_NAME>", # The person which is responsible for running the instance (at best your SE username, so people know who to ping)
+    "botMachine": "<YOUR_MACHINE_NAME>", # The system the bot runs on (example: UbuntuServer).
+    "botVersion": "v0.10.0", # The current version of the Bot, be sure to read the wiki on how to increment the version
+    "room": 163468, # The ID for the chatroom we work with
+    "chatHost": "stackoverflow.com", # The site where the bot runs. Please note that the bot has only been tested in rooms on stackoverflow.com
+    "email": "", # The credentials to log in a user which posts the messages
+    "password": "", # IMPORTANT: DO NOT COMMIT YOUR CHANGES IF YOUR CREDENTIALS ARE STILL IN HERE
+    "stackExchangeApiKey": "K8pani4F)SeUn0QlbHQsbA((", # We shouldn't run out of quota, as (directly from StackApps, not from me).
+    "redundaKey": "" # Just leave this clear unless you're sure what you're doing. If you think you need a key from me, we can discuss it in chat.
+}
+
+prod_config = {
+    "botParent": "chade_",
+    "botMachine": "UbuntuServer",
+    "botVersion": "v0.10.0",
+    "room": 111347,
+    "chatHost": "stackoverflow.com",
+    "email": "",
+    "password": "",
+    "stackExchangeApiKey": "K8pani4F)SeUn0QlbHQsbA((",
+    "redundaKey": ""
+}
