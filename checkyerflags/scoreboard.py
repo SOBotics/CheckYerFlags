730d10824741aa374e568a01561fc6fb627edee8
Filnor <philipp.bergamin@gmail.com>
2019-01-10 10:08:24 +0100

    Fixed data storing issue

--- checkyerflags/scoreboard.py
+++ checkyerflags/scoreboard.py
@@ -7,7 +7,7 @@ def update_scoreboard(flag_count, user):
     Call the scoreboard api to update the flag count for a user
     """
 
-    #Check if service account key is provided, otherwise exits
+    #Check if service account key is provided, otherwise return
     try:
         if not os.path.isfile("./service_account_key.json"):
             return
@@ -16,12 +16,9 @@ def update_scoreboard(flag_count, user):
         if flag_count <= 499:
             return
 
-        existing_record = False
-        doc_id = None
-
         #Set flag count
-        ref = db.reference("/scores")
-        ref.set({user.id: flag_count})
+        scores = db.reference("/scores")
+        scores.child(str(user.id)).set(flag_count)
 
     except BaseException as e:
         auto_logger.error(e)
