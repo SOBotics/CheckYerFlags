b961de182964c1cdae7e9e4c30bfa9fd191bebe1
Philipp <philipp.bergamin@gmail.com>
2020-07-21 08:28:41 +0200

    Remove RoomLeaver
    Is not required at all and also doesn't work

--- src/main/java/ch/philnet/checkyerflags/commands/StopCommand.java
+++ src/main/java/ch/philnet/checkyerflags/commands/StopCommand.java
@@ -1,6 +1,5 @@
 package ch.philnet.checkyerflags.commands;
 
-import ch.philnet.checkyerflags.utils.RoomLeaver;
 import org.slf4j.Logger;
 import org.sobotics.chatexchange.chat.Room;
 
@@ -22,7 +21,7 @@ public class StopCommand extends Command {
     @Override
     public void run(long messageId) {
         if (super.hasPrivileges(messageId)) {
-            room.send("Over and out!").thenRun(new RoomLeaver(room));
+            room.send("I'll be back!");
             logger.info("Stopping the bot");
             room.leave();
             System.exit(0);
